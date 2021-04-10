869ee66154a1e8a99aba96b59ba32ef2b205d953
Hoybaby <mbartek436@gmail.com>
2021-04-10 01:25:36 -0400

    changed the simple routes and made them work on my system for now

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Blog Home </h1>')
+
+def about(request):
+    # have to take in a request
+    return HttpResponse('<h1>Blog About</h2>')
