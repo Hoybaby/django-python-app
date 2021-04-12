28ff9f1f70f3b0df58c092919fb39015d138e11e
Hoybaby <mbartek436@gmail.com>
2021-04-12 01:43:10 -0400

    added some sample data to display onto my html page with the views. I am using the Jinja Template that comes with python and at first wasnt working because of spaces i had in the {% for post in post % }. The brackets need to be tight after the % to make sure it works properly.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
+# posts is a dictionary to test some data input
+posts = [
+    {
+        'author': 'Michael Bartek',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 12, 2021'
+    },
+    {
+        'author': 'Stephany Benitez',
+        'title': 'Blog Post 2',
+        'content': '2nd post content',
+        'date_posted': 'April 12, 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
     # third paramater could be placed which is context, a way to pass information into our template
 
 def about(request):
