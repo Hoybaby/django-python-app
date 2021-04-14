841ae10aa8ff3722218731a089a0a5b6b11936e6
Hoybaby <mbartek436@gmail.com>
2021-04-14 14:53:25 -0400

    installed a new package which waas crispy forms which takes care of the forms styling. Also to add the email segment to a form, i had to create a new py file that takes the older creationfrom from django and add it in. Because i am using a new form to hand the stling, i have to load these on the html which I did

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
+# this file is to add a input into the creation form so that we can add an email adress onto the register form
 class UserRegisterForm(UserCreationForm):
-    
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
