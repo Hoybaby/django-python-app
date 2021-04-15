b939990f27c018fc46dd5d2dfc6fd11fcbc2920f
Hoybaby <mbartek436@gmail.com>
2021-04-15 13:22:17 -0400

    was trouble shotting my pip module error and finally figured out why so i uncommented the commented code to bring it back then make a new picture onto my profile

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ packages to install
 pip install django-crispy-forms - allows styling of the forms
 pip install django
 pip install pillow - this is to allow python to work with images
+
+
+
+RAN 
--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 # this on delete i used on user that if the user is delete, the posts and now the PROFILE will be deleted.
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     # this will make the directory for where the pictures go
-#     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # this will make the directory for where the pictures go
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-#     # after this we need to have a dunder str method, anytime we print out a profile, it will just say profile object
-#     def __str__(self):
-#         return f'{self.user.username} Profile'
+    # after this we need to have a dunder str method, anytime we print out a profile, it will just say profile object
+    def __str__(self):
+        return f'{self.user.username} Profile'
