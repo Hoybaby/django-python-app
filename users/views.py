33d2ed382c0f3327ff76d7cd4507461631be9de8
Hoybaby <mbartek436@gmail.com>
2021-04-16 00:24:09 -0400

    i noticed that when a user was made and tried to log in, it was allowing it log in. after trouble shooting it, i realized i forgot a line of code which was to form.save()

--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
-
+from .forms import UserRegisterForm
 # Create your views here.
 
 def register(request):
@@ -11,6 +10,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in!')
             return redirect('login')
