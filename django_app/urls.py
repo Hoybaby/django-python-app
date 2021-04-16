bbb54a9367c985837ac26bda79eee808db8f9dc5
Hoybaby <mbartek436@gmail.com>
2021-04-16 15:32:27 -0400

    trying setting a standard picture as a default jpg but the default jpg would not work for the longest time. It did not work because I thought if i posted the image there with a descriptive name, the picture would take as the default but the picture ITSELF NEEDS TO BE CALLED DEFAULT.jpg for python to use it as a default.

--- django_app/urls.py
+++ django_app/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 # the include will help tell django which will go to the blog urls
 # any time you import views, we are importing mutiple views into these urls so we want to say as something else
@@ -32,8 +32,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
