5f7332444f90bbf60021560ae5eadc1bada99095
Hoybaby <mbartek436@gmail.com>
2021-04-19 10:43:57 -0400

    took all of the code from the register.html and placed it intp this html file where the CreateView class took over and restructed the crispy form that i used before. THis is why class based views can be powerful.

--- blog/urls.py
+++ blog/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 # Django makes it possible to have variabales in our path and we can tell DJango what to expect like an integer
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
 
