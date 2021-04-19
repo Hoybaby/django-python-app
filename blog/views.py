2b518324290b756ad27ccfd24a52062d36e5b915
Hoybaby <mbartek436@gmail.com>
2021-04-19 00:09:00 -0400

    like twitter, we want the most recent post to be at top so we can do this with ordering in the class and put a - infront of the categroy we named earlier

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name ='posts'
+    ordering = ['-date_posted']
     
 def about(request):
     # have to take in a request
